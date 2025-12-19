3f1eca28d0cafd67df510049f3e2b1b75fb69741
Murat <kasimrize@gmail.com>
2025-12-19 11:32:19 +0300

    Improve render-blocking handling

--- app/fonts.js
+++ app/fonts.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 export const inter = Inter({
   subsets: ["latin", "latin-ext"],
   display: "swap",
-  preload: true,          
+  preload: true,
   adjustFontFallback: true,
+  variable: "--font-inter",
 });
