d4aa69ed91cdaa769eff59b25f2bd3b16205fab0
Murat <kasimrize@gmail.com>
2025-12-19 12:38:46 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 
-import CriticalAssets from "@/components/CriticalAssets";
 import { inter } from "./fonts";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="tr" className={inter.variable} suppressHydrationWarning>
-     <CriticalAssets />    
-      <body>
+         <body>
       {children}
       </body>
     </html>
