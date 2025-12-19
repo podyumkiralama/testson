82b369c59b21be6310980ae3c9eefe629c7535d5
Murat <kasimrize@gmail.com>
2025-12-19 09:51:22 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
 // app/layout.js
 import "../styles/globals.css";
 
-import AnalyticsConsentWrapper from "@/components/AnalyticsConsentWrapper.client";
-
 export default function RootLayout({ children }) {
   return (
-    <html suppressHydrationWarning>
-      <body>
-        <AnalyticsConsentWrapper>{children}</AnalyticsConsentWrapper>
-      </body>
+    <html lang="tr" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
