2cd179caa8091f850a44a971edaeb2d25143aee4
Murat <kasimrize@gmail.com>
2025-12-19 10:00:50 +0300

    Update layout.js

--- app/(tr)/layout.js
+++ app/(tr)/layout.js
@@ -1,4 +1,15 @@
 // app/(tr)/layout.js
+import SkipLinks from "@/components/SkipLinks";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export default function TrLayout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      <SkipLinks />
+      <Navbar />
+      <main id="_main_content">{children}</main>
+      <Footer />
+    </>
+  );
 }
