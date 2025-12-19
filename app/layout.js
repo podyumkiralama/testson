7489fd302d190b7084dabdcac7c0e7e93eb3fdc7
Murat <kasimrize@gmail.com>
2025-12-19 14:06:38 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
+// app/layout.js
 import "../styles/globals.css";
-
-import { inter } from "./fonts";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr" className={inter.variable} suppressHydrationWarning>
-         <body>
-      {children}
-      </body>
+    <html lang="tr" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
