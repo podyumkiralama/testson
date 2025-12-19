a51bfc9d4bd5d4ebd97de77a46f42c1012f739f7
Murat <kasimrize@gmail.com>
2025-12-19 11:40:14 +0300

    Update layout.js

--- app/(tr)/layout.js
+++ app/(tr)/layout.js
@@ -1,6 +1,7 @@
 // app/(tr)/layout.js
 import SkipLinks from "@/components/SkipLinks";
-
+import NonCriticalStylesheet from "@/components/NonCriticalStylesheet";
+import DeferredSpeedInsights from "@/components/DeferredSpeedInsights.client";
 import NewTabAccessibility from "@/components/NewTabAccessibility.client";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -151,8 +152,10 @@ export default function TrLayout({ children }) {
   return (
     <>
       <SkipLinks />
+    <NonCriticalStylesheet />
 
-  
+      <DeferredSpeedInsights />
+ 
       <NewTabAccessibility />
 
       <TrSiteJsonLd />
